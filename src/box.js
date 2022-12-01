568040c9b5910f2575ba441389759ef2b6e28004
nagysandordominik <thesatyka@gmail.com>
2022-12-01 20:25:42 +0100

    add: rendering box based on setState and binding handleclick

---
+++ src/box.js
@@ -0,0 +1,25 @@
+import React, { Component } from "react";
+import './box.css';
+
+class Box extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {color: 'purple'};
+        this.handleClick = this.handleClick.bind(this);
+    }
+    handleClick(){
+
+    }
+    render() {
+        return (
+            <div className="Box" 
+            style={{backgroundColor: this.state.color}} 
+            onClick={this.handleClick}>
+
+            </div>
+        )
+    }
+}
+
+
+export default Box;
