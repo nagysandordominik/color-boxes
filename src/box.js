1ec5851695572a5880e4e55bc98f9f18084fb9ad
nagysandordominik <thesatyka@gmail.com>
2022-12-01 20:57:32 +0100

    add: assign multiple colors to defaultProps

--- src/box.js
+++ src/box.js
@@ -2,13 +2,20 @@ import React, { Component } from "react";
 import './box.css';
 
 class Box extends Component {
+    static defaultProps = {
+        allColors: ["purple", "magenta", "pink", "levander"]
+    }
     constructor(props) {
         super(props);
         this.state = {color: 'purple'};
         this.handleClick = this.handleClick.bind(this);
     }
+    pickColor(){
+        
+    }
+    
     handleClick(){
-
+        this.pickColor()
     }
     render() {
         return (
