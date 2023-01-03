a9e556f52a9a90b9b2a19655840414893a5e3d71
nagysandordominik <thesatyka@gmail.com>
2023-01-03 23:42:54 +0100

    remove: allColors property

--- src/box.js
+++ src/box.js
@@ -3,18 +3,15 @@ import './box.css';
 import { choice } from "./helpers";
 
 class Box extends Component {
-    static defaultProps = {
-        allColors: ["purple", "magenta", "pink", "blue", "violet"]
-    }
     constructor(props) {
         super(props);
-        this.state = {color: choice(this.props.allColors)};
+        this.state = {color: choice(this.props.colors)};
         this.handleClick = this.handleClick.bind(this);
     }
     pickColor(){
         let newColor;
         do {
-            newColor = choice(this.props.allColors);
+            newColor = choice(this.props.colors);
         } while (newColor === this.state.color);
         
         this.setState({color: newColor});
