1aa705695d85a7d86e02d72f16ff8751bb7bad02
nagysandordominik <thesatyka@gmail.com>
2023-01-03 23:42:36 +0100

    add:boxContainer.js imported

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
+import React, {Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
-import Box from './box';
+import BoxContainer from './boxContainer';
 
-function App() {
-  return (
-  <Box/>
-  );
+class App extends Component {
+  render() {
+    return (
+      <div className='App'>
+        <BoxContainer />
+      </div>
+    );
+  }
+  
 }
 
 export default App;
